376d84455aeec7cd495afe9bc7dda9925167a045
PrabhaSDET-TestV <prabhakaranr@testvagrant.com>
2025-05-09 15:31:08 +0530

    Update global-setup.ts

--- global-setup.ts
+++ global-setup.ts
@@ -1,32 +1,11 @@
-// global-setup.ts
-import axios from 'axios';
 import fs from 'fs/promises';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const BASE_URL = process.env.BASE_URL!;
-const PROJECT_ID = process.env.PROJECT_ID!;
-const API_KEY = process.env.API_KEY!;
-
-const HEADERS = {
-  'x-api-key': API_KEY,
-  'Content-Type': 'application/json',
-  Accept: 'application/json',
-};
+import { BuildService } from './src/lib/build-service';
 
 async function globalSetup() {
-  const createUrl = `${BASE_URL}/projects/${PROJECT_ID}/builds?days=7&environment=production`;
-  const payload = {
-    duration: 0,
-    environment: 'production',
-    status: 'in_progress',
-  };
+  const build = await BuildService.startBuild();
+  const buildId = build.build_id;
 
-  const res = await axios.post(createUrl, payload, { headers: HEADERS });
-  const buildId = res.data.build.build_id;
-
-  console.log('✅ Global Setup: Build started:', buildId);
+  console.log('Global Setup: Build started:', buildId);
 
   // Save buildId to a temp file
   await fs.writeFile('build-meta.json', JSON.stringify({ buildId }, null, 2));
